4fa6dd7c95c4fad37cde79af19b272d1048a313d
Govi2020 <82364234+Govi2020@users.noreply.github.com>
2025-08-16 00:25:00 +0530

    Made Compatable with New Shazam API

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,6 +1,6 @@
 import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
-// let api_key = 
+// TODO: Remove country_code=${"IN"} and replace it with the api result
 
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
@@ -12,8 +12,8 @@ export const shazamCoreApi = createApi({
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({query: () => (`/charts/world?country_code=${countrycode}`)}),
-        getSongsByGenre: builder.query({query: (({genreListId}) => (`/charts/genre-world?genre_code=${genreListId.toUpperCase()}&country_code=${countrycode}`))}),
+        getTopCharts: builder.query({query: () => (`/charts/world?country_code=${"IN"}`)}),
+        getSongsByGenre: builder.query({query: (({genreListId}) => (`/charts/genre-world?genre_code=${genreListId.toUpperCase()}&country_code=${"IN"}`))}),
         getSongDetails: builder.query({query: (({songid}) => (`/tracks/details?track_id=${songid}`))}),
         getSongRelated : builder.query({query: (({songid}) => (`/tracks/related?track_id=${songid}`))}),
         getArtistDetails : builder.query({query: ((artistid) => (`/artists/details?artist_id=${artistid}`))}),
