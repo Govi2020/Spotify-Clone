c8cf0ec336c1ec039804cf35b3de90642c15ff93
Govi2020 <82364234+Govi2020@users.noreply.github.com>
2025-08-16 00:17:38 +0530

    Update shazamCore.js

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,8 +12,8 @@ export const shazamCoreApi = createApi({
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({query: () => ("/charts/world")}),
-        getSongsByGenre: builder.query({query: (({genreListId}) => (`/charts/genre-world?genre_code=${genreListId.toUpperCase()}`))}),
+        getTopCharts: builder.query({query: () => (`/charts/world?country_code=${countrycode}`)}),
+        getSongsByGenre: builder.query({query: (({genreListId}) => (`/charts/genre-world?genre_code=${genreListId.toUpperCase()}&country_code=${countrycode}`))}),
         getSongDetails: builder.query({query: (({songid}) => (`/tracks/details?track_id=${songid}`))}),
         getSongRelated : builder.query({query: (({songid}) => (`/tracks/related?track_id=${songid}`))}),
         getArtistDetails : builder.query({query: ((artistid) => (`/artists/details?artist_id=${artistid}`))}),
